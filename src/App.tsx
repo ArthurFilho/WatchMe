2c1dec7e83d354fe0dab2d77bb4b35cd7ede1ec5
Arthur <peraarthur2018@gmail.com>
2023-02-18 14:22:50 -0300

    styles + styled components add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from "./Header";
 
 export function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+
+      
+    </>
   )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,17 @@
+import { ContainerHeader } from "./styles/header/styles";
+
+export function Header () {
+return (
+    <ContainerHeader>
+        <h1>WatchMe</h1>
+        <ul>
+            <li> Ação </li>
+            <li> Comédia </li>
+            <li> Documentario </li>
+            <li> Drama </li>
+            <li> Terror </li>
+            <li> Família </li>
+        </ul>
+    </ContainerHeader>
+    )
+}
