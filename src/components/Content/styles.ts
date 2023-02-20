cfcd55be6921db3ea50650d60cf6c20cf79d48b0
ArthurFilho <peraarthur2018@gmail.com>
2023-02-20 20:25:16 -0300

    styles movie

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -13,24 +13,37 @@ export const Movie = styled.div`
     display: flex;
     flex-wrap: wrap;
     margin-top: 100px;
+    
     section {
         display: flex;
         background-color: white;
+        position: relative;
         width: 200px;
         height: 250px;
         margin-right: 20px;
         color: black;
+        box-shadow:0 1px 2px 0 rgba(0,0,0,0.1);
+        transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+        
         div {
             display: flex;
             flex-direction: column;
-            position: relative;
+            position: absolute;
             color: white;
+            margin-top: 200px;
+            margin-left: 30px;
+            
+            h4 {
+                margin-bottom: 5px;
+            }
+            
             ul {
                 display: flex;
                 gap: 5px;
                 align-items: center;
                 justify-content: space-around;
             }
+            
             li {
                 display: flex;
                 gap: 3px;
@@ -38,8 +51,13 @@ export const Movie = styled.div`
                 align-items: center;
             }
         }
+        
         img {
             width: 100%;
+            position: relative;
         }
     }
+        section:hover{
+            transform:scale(1.15,1.15);
+            }
 `
